cb00a3dcd3ee45143d5023aad7bf65aa8c761df9
Brig Young <Sonophoto@users.noreply.github.com>
2016-03-11 12:47:43 -0800

    Update shellnoise.c

--- shellnoise.c
+++ shellnoise.c
@@ -1,18 +1,18 @@
 /* shellnoise.c -- VERSION 0.0.1
  *
- *                 _          _ _             _          
- *  BSD /      ___| |__   ___| | |_ __   ___ (_)___  ___ 
- *     / C99  / __| '_ \ / _ \ | | '_ \ / _ \| / __|/ _ \
- *            \__ \ | | |  __/ | | | | | (_) | \__ \  __/
- *            |___/_| |_|\___|_|_|_| |_|\___/|_|___/\___|
+ *                   _          _ _             _          
+ *  BSD /        ___| |__   ___| | |_ __   ___ (_)___  ___ 
+ *     / C99    / __| '_ \ / _ \ | | '_ \ / _ \| / __|/ _ \
+ *              \__ \ | | |  __/ | | | | | (_) | \__ \  __/
+ *              |___/_| |_|\___|_|_|_| |_|\___/|_|___/\___|               
  *                                          
  *
- *             A BSD Licensed Guerrilla Shell Library 
- *   Against the idea that a shell lib needs 20,000K lines of C code.
+ *                 A BSD Licensed Guerrilla Shell Library 
+ *     Against the idea that a shell lib needs 20,000K lines of C code.
  *
  * You can find the latest source code at:
  *
- *               http://github.com/sonophoto/shellnoise
+ *                 http://github.com/sonophoto/shellnoise
  *
  * Makes a number of crazy assumptions that happen to be true in 99.9942%
  * of the 2010 - 2016 unix/POSIX/GNU/*Linux/*BSD computers around.
@@ -21,6 +21,7 @@
 /* ------------------------------------------------------------------------
  * B S D  T W O  C L A U S E  L I C E N S E  A N D  C O P Y R I G H T S
  * ------------------------------------------------------------------------
+ * Original Linenoise Functions:
  * Copyright (c) 2010-2014, Salvatore Sanfilippo <antirez at gmail dot com>
  * Copyright (c) 2010-2013, Pieter Noordhuis <pcnoordhuis at gmail dot com>
  *
@@ -29,6 +30,8 @@
  *
  * Additional Shell Functions:
  *  Copyright (c) 2015-2016, Brig Young [github.com/sonophoto]
+ *
+ * Names have been changed to protect namespaces.
  *
  *                ----- All rights reserved. -----
  *
